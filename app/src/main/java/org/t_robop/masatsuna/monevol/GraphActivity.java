ee8811de8e541f60ef976009b33d0ea1ffbb0e80
otogin <hagakuresentyou@i.softbank.jp>
2016-06-21 16:37:11 +0900

    t

--- app/src/main/java/org/t_robop/masatsuna/monevol/GraphActivity.java
+++ app/src/main/java/org/t_robop/masatsuna/monevol/GraphActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 import android.view.Window;
 
 public class GraphActivity extends AppCompatActivity {
-
+//aaa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
